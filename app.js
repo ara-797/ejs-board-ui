a856e1a47908416af54fb0ee868b45956ec02488
ara-797 <>
2025-12-19 15:32:13 +0900

    페이지 분리 및 api 추가

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ app.set('view engine', 'ejs');
 // 정적 파일(CSS) 설정
 app.use('/assets', express.static('assets'));
 
+const todoList = [
+  { id: 1, text: '해야할 일 01', done: true },
+  { id: 2, text: '해야할 일 02', done: false },
+  { id: 3, text: '해야할 일 03', done: false },
+  { id: 4, text: '해야할 일 04', done: false },
+  { id: 5, text: '해야할 일 05', done: true }
+];
+
+// HOME
 app.get('/', (req, res) => {
-  res.render('index', {
-    todos: ['첫 번째 할 일', '두 번째 할 일']
-  });
+  res.render('index', { pageTitle: 'HOME 화면' });
+});
+
+// TODO
+app.get('/todo', (req, res) => {
+  res.render('todo', { todoList, pageTitle: 'TODO 리스트' });
 });
 
 app.listen(3000, () => {
